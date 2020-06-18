d7734f18474af104a09a7da495c52178197f2857
swag31415 <kidswagatam18@gmail.com>
2020-06-18 10:45:23 -0400

    Pulled unniversal project things into quickproj.nim

--- src/quickproj.nim
+++ src/quickproj.nim
@@ -1,3 +1,31 @@
 ## Main
 
+import os
+import strutils
+
+import utils
 import nim/quicknim
+
+# Get name of project (no spaces) and project description
+let name = ask("Enter Project Name").replace(' ', '_')
+let desc = ask("Enter Project Desc")
+
+# Gets path to project on desktop
+let dir = expandTilde("~\\Desktop\\" & name)
+
+# Creates project directory
+if existsOrCreateDir(dir): 
+  # Exits if project directory already exists
+  echo name, " already exists"
+  wait_and_exit()
+
+# Sets the current directory to the project directory
+setCurrentDir(dir)
+
+quicknim.run(name, desc)
+
+# Open VS Code
+run("code -n .")
+  
+# All done!
+wait_and_exit()
